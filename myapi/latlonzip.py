ee4df1707beeae3a33f6dca0678e637fa3a9abce
jsw68 <jwainberg68@gmail.com>
2021-02-19 22:32:38 +0000

    resolved conflicts

--- myapi/latlonzip.py
+++ myapi/latlonzip.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from uszipcode import SearchEngine
 
 
@@ -8,14 +7,3 @@ def get_lat_lon_zip(code):
     res_dict = results.to_dict()
     lat, lon = res_dict['lat'], res_dict['lng']
     return lat, lon
-=======
-from uszipcode import SearchEngine
-
-
-def get_lat_lon_zip(code):
-    search = SearchEngine()
-    results = search.by_zipcode(code)
-    res_dict = results.to_dict()
-    lat, lon = res_dict['lat'], res_dict['lng']
-    return lat, lon
->>>>>>> 99b250db5b49285a599972d16c2118e8f1173793
