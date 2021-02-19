a98e28e190a82d2f9767a1a0bcba65655dd0df1c
jsw68 <jwainberg68@gmail.com>
2021-02-19 22:17:48 +0000

    Chnaged Zip code finder. Added search link to homepage

--- myapi/latlonzip.py
+++ myapi/latlonzip.py
@@ -1,9 +1,9 @@
-import pgeocode
+from uszipcode import SearchEngine
 
 
 def get_lat_lon_zip(code):
-    nominatim = pgeocode.Nominatim('us')
-    table = nominatim.query_postal_code(code)
-    lat = table['latitude']
-    lon = table['longitude']
+    search = SearchEngine()
+    results = search.by_zipcode(code)
+    res_dict = results.to_dict()
+    lat, lon = res_dict['lat'], res_dict['lng']
     return lat, lon
